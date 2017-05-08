091ee7016ce5127d76e17e7cbdd4c63bdf5c2b5b
busydoingnothing <kajetan88@o2.pl>
2017-05-08 21:43:53 +0200

    3
    font colors and general styling

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-(function(){if (document.location.hash == "" || document.location.hash == "#")
-    document.location.hash = "#front";
+(function () {
+    if (document.location.hash === "" || document.location.hash === "#") {
+        document.location.hash = "#front";
 
-    var isChrome = !!window.chrome && !!window.chrome.webstore;
+        var isChrome = !!window.chrome && !!window.chrome.webstore;
 
-    var mixBlends = document.querySelectorAll('.nav__item');
+        var mixBlends = document.querySelectorAll(".nav__item");
 
-    if (isChrome){
-        mixBlends.forEach(function(obj){
-            obj.style.mixBlendMode = 'normal';
-            obj.style.color = 'black';
-        });
-}})();
+        if (isChrome) {
+            mixBlends.forEach(function (obj) {
+                obj.style.mixBlendMode = "normal";
+                obj.style.color = "black";
+            });
+        }
+    }
+})();
