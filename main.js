9d42742079d2508fd05b58013faf1f011411d7e4
engineering-this <kajetan88@o2.pl>
2017-05-16 18:58:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,66 +3,58 @@
     if (document.location.hash === "" || document.location.hash === "#") {
         window.history.replaceState("", "", "?lang=pl#front");
         document.location = "?lang=pl#front";
-        var isChrome = !!window.chrome && !!window.chrome.webstore;
-
-        var mixBlends = document.querySelectorAll(".nav__item");
-
-        if (isChrome) {
-            mixBlends.forEach(function (obj) {
-                obj.style.mixBlendMode = "normal";
-                obj.style.color = "black";
-            });
-        }
     }
 
-    function getQueryVariable(variable)
-    {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            if(pair[0] == variable){return pair[1];}
+    let isChrome = !!window.chrome && !!window.chrome.webstore;
+    const mixBlends = document.querySelectorAll(".nav__item");
+
+    if (isChrome) {
+        mixBlends.forEach(function (obj) {
+            obj.style.mixBlendMode = "normal";
+            obj.style.color = "black";
+        });
+    }
+
+    let getQueryVariable = function (variable){
+        let vars = window.location.search.substring(1).split("&");
+        for (let item of vars) {
+            if(item.split("=")[0] == variable){return item.split("=")[1];}
         }
         return(false);
-    }
-    var langEn = JSON.parse(langen);
-    var langPl = JSON.parse(langpl);
+    };
 
-    var switchLangInAdr = function (lang){
+    const langEn = JSON.parse(langen),
+        langPl = JSON.parse(langpl);
+
+    const switchLangInAdr = function (lang){
         window.history.pushState("", "", `?lang=${lang}${document.location.hash}`)
     };
 
-    var currentLang = getQueryVariable('lang');
+    let currentLang = getQueryVariable('lang');
 
-    var langBtn = document.querySelector(".nav__item--lang");
+    const langBtn = document.querySelector(".nav__item--lang"),
+        cubeSides = ["front", "back", "right", "top", "left", "bottom"],
+        cubeHeadings = cubeSides.map(item=>{return document.querySelector(`.cube__${item} h1`)}),
+        cubeText = cubeSides.filter((item)=>{if (item != 'left'){return item}}).map(item=>{return document.querySelector(`.cube__${item} p`)}),
+        leftHeadings = [...document.querySelectorAll('.cube__left h2')],
+        leftText = [...document.querySelectorAll('.cube__left p')];
 
 
-
-    var cubeSides = ["front", "back", "right", "top", "left", "bottom"];
-    var cubeHeadings = [];
-    var cubeText = [];
-    var leftHeadings = [...document.querySelectorAll('.cube__left h2')];
-    var leftText = [...document.querySelectorAll('.cube__left p')];
-
-    var htmlLangSwitch = function (currentLang) {
-        for(i=0; i< cubeHeadings.length; i++) {
-            cubeHeadings[i].innerText = currentLang.headings[i];
-        }
-
-        for(i=0; i< cubeText.length; i++) {
-            cubeText[i].innerHTML = currentLang.txt[i];
-        }
-
-        for(i=0; i< leftHeadings.length; i++) {
-            leftHeadings[i].innerText = currentLang.headingsleft[i];
-        }
-
-        for(i=0; i< leftText.length; i++) {
-            leftText[i].innerHTML = currentLang.textleft[i];
-        }
+    const arrayIntoHtml = function (sourceArray, targetArray){
+            for(let i = 0 ; i < targetArray.length; i++) {
+                targetArray[i].innerHTML = sourceArray[i];
+            }
     };
 
-    var langSwitch = function () {
+
+    const htmlLangSwitch = function (currentLang) {
+        arrayIntoHtml(currentLang.headings, cubeHeadings);
+        arrayIntoHtml(currentLang.txt, cubeText);
+        arrayIntoHtml(currentLang.headingsleft, leftHeadings);
+        arrayIntoHtml(currentLang.textleft, leftText);
+    };
+
+    const langSwitch = function () {
         if (currentLang === 'pl'){
             currentLang = 'en';
             switchLangInAdr(currentLang);
@@ -73,22 +65,11 @@
             htmlLangSwitch(langPl);
         }
 
-        langBtn.classList.toggle('nav__item--lang-pl')
-        langBtn.classList.toggle('nav__item--lang-en')
+        langBtn.classList.toggle('nav__item--lang-pl');
+        langBtn.classList.toggle('nav__item--lang-en');
     };
 
-
     langBtn.addEventListener('click', langSwitch);
-
-
-
-    cubeSides.forEach(function(item){
-        cubeHeadings.push(document.querySelector(`.cube__${item} h1`));
-        if (item != 'left') {
-            cubeText.push(document.querySelector(`.cube__${item} p`));
-        }
-    });
-
 
     if (currentLang === 'pl') {
         langBtn.classList.add('nav__item--lang-pl');
@@ -100,7 +81,6 @@
         langBtn.classList.add('nav__item--lang-en');
         langBtn.classList.remove('nav__item--lang-pl');
         htmlLangSwitch(langEn);
-        console.log(currentLang);
     }
 
 })();
